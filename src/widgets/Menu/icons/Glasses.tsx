d56efa032a8a504070000427297a76623f548d09
TomYumGoongAdmin <tomyumgoong.finance@gmail.com>
2021-06-17 23:25:53 +0700

    add check lp

--- src/widgets/Menu/icons/Glasses.tsx
+++ src/widgets/Menu/icons/Glasses.tsx
@@ -5,12 +5,8 @@ import { SvgProps } from "../../../components/Svg/types";
 const Icon: React.FC<SvgProps> = (props) => {
   return (
     <Svg viewBox="0 0 512 512" {...props}>
-      <g>
-        <g>
-          <circle cx="442.5" cy="205.5" r="18.5" />
-          <circle cx="171.5" cy="205.5" r="18.5" />
-        </g>
-      </g>
+      <circle cx="442.5" cy="205.5" r="18.5" />
+      <circle cx="171.5" cy="205.5" r="18.5" />
       <circle cx="126.9" cy="249.4" r="113.9" />
       <circle cx="384.3" cy="249.4" r="113.9" />
     </Svg>
